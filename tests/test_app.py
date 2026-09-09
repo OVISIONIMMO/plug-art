e160319c76e7d4cd3109d86276fe0787da9c0475
OVISIONIMMO <pintor.florian.art@gmail.com>
2026-09-09 15:32:53 +0200

    Corrige l’import des tests CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,8 @@
-import os
+import os, sys
 from pathlib import Path
 
+ROOT=Path(__file__).resolve().parents[1]
+sys.path.insert(0,str(ROOT))
 TEST_DB='/tmp/plugart_test.db'
 os.environ['PLUGART_DB']=TEST_DB
 os.environ['PLUGART_AUTORADAR']='0'
