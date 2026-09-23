b4e21b10088cb0df57d4d44fb9613fe03371f9ed
OVISIONIMMO <pintor.florian.art@gmail.com>
2026-09-23 18:22:13 +0200

    PLUG ART V111.2: finalize merged fast mobile runtime

--- static/plugart_v107.js
+++ static/plugart_v107.js
@@ -1,6 +1,6 @@
 (function(){
 'use strict';
-const VERSION='110.20260923.5';
+const VERSION='111.20260923.2';
 const $=(s,r=document)=>r.querySelector(s), $$=(s,r=document)=>Array.from(r.querySelectorAll(s));
 const esc=v=>String(v??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
 const clean=v=>String(v??'').replace(/\s+/g,' ').trim();
