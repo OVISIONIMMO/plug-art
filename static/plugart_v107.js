e33c78972197c6273117b9f18609374185286d7c
OVISIONIMMO <pintor.florian.art@gmail.com>
2026-09-23 18:14:37 +0200

    PLUG ART V110: align mobile runtime version

--- static/plugart_v107.js
+++ static/plugart_v107.js
@@ -1,6 +1,6 @@
 (function(){
 'use strict';
-const VERSION='109.20260923.4';
+const VERSION='110.20260923.1';
 const $=(s,r=document)=>r.querySelector(s), $$=(s,r=document)=>Array.from(r.querySelectorAll(s));
 const esc=v=>String(v??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
 const clean=v=>String(v??'').replace(/\s+/g,' ').trim();
