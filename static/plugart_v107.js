476aae6fc18aab44aca7f214f808e4a4c676512b
OVISIONIMMO <pintor.florian.art@gmail.com>
2026-09-25 19:22:48 +0200

    PLUG ART V151.1: finalize unified Studio runtime version

--- static/plugart_v107.js
+++ static/plugart_v107.js
@@ -1,6 +1,6 @@
 (function(){
 'use strict';
-const VERSION='150.20260925.1';
+const VERSION='151.20260925.1';
 const $=(s,r=document)=>r.querySelector(s), $$=(s,r=document)=>Array.from(r.querySelectorAll(s));
 const esc=v=>String(v??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
 const clean=v=>String(v??'').replace(/\s+/g,' ').trim();
