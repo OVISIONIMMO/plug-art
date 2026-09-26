2b9e9d9b15b3a9505cd4f50094469c5397614c01
OVISIONIMMO <pintor.florian.art@gmail.com>
2026-09-26 03:14:49 +0200

    PLUG ART V161.3: remove literal newline escape from startup log

--- app_extra_v59.py
+++ app_extra_v59.py
@@ -2925,7 +2925,8 @@ def status_v90():
       'background':'free translucent internal workspace with standalone PLUGY, free canvas Creation, HUB project workspace, functional opportunity map, social studio and integrated creative tools'
     }
 
-print("PLUG_ART_V161_READY ui=immersive_workspace plugy=expanded_stage_safe_arms creation=live_preview visual=chat_to_bureau radar=photo_hospitality_no_contests artists=profile_crm_suggestions",flush=True)\n
+print("PLUG_ART_V161_READY ui=immersive_workspace plugy=expanded_stage_safe_arms creation=live_preview visual=chat_to_bureau radar=photo_hospitality_no_contests artists=profile_crm_suggestions",flush=True)
+
 def _v127_runtime_smoke():
     required_routes={
       ('GET','/api/health'),
